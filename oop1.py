c8130d57e977fd1e54d6d1a6d90d1496e7d617b1
FreyGeospatial <jordan@freygeospatial.com>
2022-03-23 21:01:58 -0400

    finished last exercise of tutorial

--- oop1.py
+++ oop1.py
@@ -240,11 +240,32 @@ miles = JackRussellTerrier("Miles", 4)
 miles.speak() # "Miles barks: Arf"
 
 
+####################
+####################
+####################
+
+# Create a GoldenRetriever class that inherits from the Dog class. 
+# Give the sound argument of GoldenRetriever.speak() a default value of "Bark". 
+# Use the following code for your parent Dog class:
+
+class Dog:
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+class GoldenRetriever(Dog):
+    def speak(self, sound="Bark"):
+        return super().speak(sound)
 
 
-
-
-
-
-
+ShilohCousin = GoldenRetriever("Shiloh", 1)
+ShilohCousin.speak()
 
